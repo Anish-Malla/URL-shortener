e0053552fd60226d2189e715d43c50589119b421
Anish Malla <63273493+Anish-Malla@users.noreply.github.com>
2020-09-23 12:59:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'anish'
 
 db = SQLAlchemy(app)
 
